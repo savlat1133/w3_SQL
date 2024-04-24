0edf39ddb6a1cdc8caa5bf3bc721f793e2f416dd
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-04-24 21:50:09 +0100

    Create Exercise23.sql

---
+++ Retrieve-Data-From-Tables/Exercise23.sql
@@ -0,0 +1,4 @@
+-- 23. From the following table, write a SQL query to find Nobel Prize winners for the subject that does not begin with the letter 'P'. Return year, subject, winner, country, and category. Order the result by year, descending and winner in ascending.  
+
+SELECT * FROM nobel_win
+WHERE subject NOT LIKE 'P%';
