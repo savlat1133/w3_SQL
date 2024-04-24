570d20d31fd2a5d49d0352644eb0583b6ad1581e
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-04-24 20:50:43 +0100

    Create Exercise7.sql

---
+++ Retrieve-Data-From-Tables/Exercise7.sql
@@ -0,0 +1,3 @@
+-- 7. Write a query to display the columns in a specific order, such as order date, salesman ID, order number, and purchase amount for all orders.  
+-- Sample table: orders
+SELECT ord_date, salesman_id, ord_no, purch_amt FROM orders;
