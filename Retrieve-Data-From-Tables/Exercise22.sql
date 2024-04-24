9143b162add4305d58964768977db0750373e1cf
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-04-24 21:48:10 +0100

    Create Exercise22.sql

---
+++ Retrieve-Data-From-Tables/Exercise22.sql
@@ -0,0 +1,2 @@
+-- 22. From the following table, write a SQL query to find the details of the Nobel Prize winner 'Johannes Georg Bednorz'. Return year, subject, winner, country, and category.  
+SELECT * FROM nobel_win WHERE winner = 'Johannes Georg Bednorz';
