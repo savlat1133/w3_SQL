7408659fb8e70de94153bd6b82f1a4f66ee862fb
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-04-24 21:11:24 +0100

    Create Exercise10.sql

---
+++ Retrieve-Data-From-Tables/Exercise10.sql
@@ -0,0 +1,4 @@
+-- 10. From the following table, write a SQL query to find customers whose grade is 200. Return customer_id, cust_name, city, grade, salesman_id.  
+-- Sample table: customer
+
+SELECT * FROM customer WHERE grade = 200;
