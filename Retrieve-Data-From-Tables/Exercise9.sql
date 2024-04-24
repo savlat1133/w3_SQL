2ad26403a15b86dda7234b09cb3dbee895f72529
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-04-24 21:09:16 +0100

    Create Exercise9.sql

---
+++ Retrieve-Data-From-Tables/Exercise9.sql
@@ -0,0 +1,4 @@
+-- 9. From the following table, write a SQL query to locate salespeople who live in the city of 'Paris'. Return salesperson's name, city. 
+-- Sample table: salesman
+
+SELECT name, city FROM salesman WHERE city = 'Paris';
