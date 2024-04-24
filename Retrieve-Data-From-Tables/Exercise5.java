4eaafb83b52360df40ed51a673510f56154ea90e
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-04-24 20:47:01 +0100

    Update and rename Exercise5.java to Exercise5.sql

--- Retrieve-Data-From-Tables/Exercise5.java
+++
@@ -1,2 +0,0 @@
-// 5. Write an SQL query to display the result of an arithmetic expression. 
-SELECT 10-15*2;
---
+++ Retrieve-Data-From-Tables/Exercise5.sql
@@ -0,0 +1,2 @@
+-- 5. Write an SQL query to display the result of an arithmetic expression. 
+SELECT 10-15*2;
