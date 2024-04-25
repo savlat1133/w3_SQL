2da22aab4bbb7890006b71403d42188be7b6b34f
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-04-25 12:18:45 +0100

    Create Exercise33.sql

---
+++ Retrieve-Data-From-Tables/Exercise33.sql
@@ -0,0 +1,4 @@
+-- 33. From the following table, write a SQL query to retrieve the details of the employees who work in the department 57. 
+-- Return emp_idno, emp_fname, emp_lname and emp_dept..  
+SELECT * FROM emp_details
+WHERE emp_dept = 57;
