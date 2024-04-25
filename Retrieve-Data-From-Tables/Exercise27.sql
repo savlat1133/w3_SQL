b1b327be9fd0b9fda5e619f19dcd30c37862036d
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-04-25 11:51:24 +0100

    Create Exercise27.sql

---
+++ Retrieve-Data-From-Tables/Exercise27.sql
@@ -0,0 +1,3 @@
+-- 27. From the following table, write a SQL query to display the pro_name as 'Item Name' and pro_priceas 'Price in Rs.'  
+SELECT pro_name AS "Item Name", pro_price AS "Price in Rs." 
+FROM item_mast;
