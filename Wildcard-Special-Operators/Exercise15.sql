22c18c3f76d40716bff7d524370e6f43c88f894d
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-05-08 21:23:45 +0100

    Create Exercise15.sql

--- Wildcard-Special-Operators/Exercise15.sql
+++ Wildcard-Special-Operators/Exercise15.sql
@@ -1,4 +1,5 @@
 -- 15. From the following table, write a SQL query to identify those rows where col1 does not contain the forward slash character ( / ). Return col1.
 
+
 SELECT * FROM testtable
 WHERE col1 NOT LIKE '%/%';
