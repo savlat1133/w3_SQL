4377c28723da281293d08d329c18fa68e65b56ce
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-05-13 19:27:59 +0100

    Create Exercise24.sql

---
+++ Aggregate-Functions/Exercise24.sql
@@ -0,0 +1,3 @@
+-- 24. From the following table, write a SQL query to compute the sum of the allotment amount of all departments. Return sum of the allotment amount.
+
+SELECT SUM(dpt_allotment) FROM emp_department;
