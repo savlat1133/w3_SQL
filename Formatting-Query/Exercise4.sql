b397b0b2ec00e66706bc6d6ee04e812da63bf222
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-05-14 16:46:12 +0100

    Create Exercise4.sql

---
+++ Formatting-Query/Exercise4.sql
@@ -0,0 +1,4 @@
+-- 4. From the following table, write a SQL query to find all the orders. Sort the result-set in descending order by ord_date. Return all fields. 
+
+SELECT * FROM orders
+ORDER BY ord_no DESC;
