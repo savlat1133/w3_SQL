952024eccb0c936f710d246127325812f5122641
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-05-14 16:51:29 +0100

    Create Exercise6.sql

---
+++ Formatting-Query/Exercise6.sql
@@ -0,0 +1,3 @@
+-- 6. From the following table, write a SQL query to find all the customers. Sort the result-set by customer_id. Return cust_name, city, grade. 
+SELECT cust_name, city, grade FROM customer
+ORDER BY customer_id;
