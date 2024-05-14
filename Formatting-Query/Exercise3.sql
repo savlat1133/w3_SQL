f7453c837308a356529ec66c200433f8d18f9e23
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2024-05-14 16:45:17 +0100

    Create Exercise3.sql

---
+++ Formatting-Query/Exercise3.sql
@@ -0,0 +1,3 @@
+-- 3. From the following table, write a SQL query to find all the orders. Sort the result-set in ascending order by ord_no. Return all fields.
+SELECT * FROM orders
+ORDER BY ord_no;
